d7b63b9dac05544aba1ede477c1338172df7f730
Alexander0127 <AlexanderTR0127@gmail.com>
2024-04-15 13:35:02 -0500

    Configuración tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'main-practica': "url('/assets/Inicio-Práctica.jpg')",
+      }
+    },
+  },
+  plugins: [],
+}
+
